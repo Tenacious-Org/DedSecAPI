1ebdd271cf787ecb6e1b530f35139f8d3d171b69
Jeevanantham N <jeevanantham3006@gmail.com>
2022-05-18 09:08:53 +0530

    Added Image in Employee Model

--- A5/Models/Employee.cs
+++ A5/Models/Employee.cs
@@ -7,39 +7,40 @@ namespace A5.Models
     public class Employee : IEntityBase, IAudit, IValidation<Employee>
     {
     
-        public int Id {get;set;}
-        public string  ACEID {get;set;}
-        public string  FirstName {get;set;}
-        public string  LastName {get;set;}
-        public string  Email {get;set;}
+        public int Id { get; set; }
+        public string  ACEID { get; set; }
+        public string  FirstName { get; set; }
+        public string  LastName { get; set; }
+        public string  Email { get; set; }
+        public byte[] ? Image{ get; set; }
         public DateTime DOB { get; set; }
-        public int OrganisationId {get;set;}
+        public int OrganisationId { get; set;}
         [ForeignKey("OrganisationId")]
-        public virtual Organisation? Organisation{get; set; }
-        public int DepartmentId {get;set;}
-        public int DesignationId {get;set;}
-        public int ? ReportingPersonId {get;set;}
+        public virtual Organisation? Organisation{ get; set; }
+        public int DepartmentId { get; set;}
+        public int DesignationId { get; set;}
+        public int ? ReportingPersonId { get; set;}
         [ForeignKey("ReportingPersonId"),NotMapped]
         
         [InverseProperty("Reportingpersons")]
         public Employee? ReportingPerson{ get; set; }
-        public int? HRId {get;set;}
+        public int? HRId { get; set; }
         [ForeignKey("HRId"),NotMapped]
         
         [InverseProperty("Hrs")]
         public Employee? HR{ get; set; }
-        public string  Password {get;set;}
-        public bool IsActive {get;set;}
-         public int AddedBy {get; set;}
-        public DateTime AddedOn{get; set;}
-        public int ? UpdatedBy {get; set;}
-        public DateTime ? UpdatedOn {get;set;}
+        public string  Password { get; set; }
+        public bool IsActive { get; set; }
+         public int AddedBy { get; set; }
+        public DateTime AddedOn{ get; set; }
+        public int ? UpdatedBy { get; set; }
+        public DateTime ? UpdatedOn { get; set; }
         
         [InverseProperty("ReportingPerson")]
-        public ICollection<Employee> ? Reportingpersons {get;set;}
+        public ICollection<Employee> ? Reportingpersons { get; set; }
         
         [InverseProperty("HR")]
-        public ICollection<Employee> ? Hrs {get;set;}
+        public ICollection<Employee> ? Hrs { get; set; }
 
 
         public bool CreateValidation(Employee employee)
