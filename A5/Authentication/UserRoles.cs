d9c6f9aa59a609c9cf891c8f1f29694232970b4e
Jeevanantham N <jeevanantham3006@gmail.com>
2022-06-06 12:22:35 +0530

    Create UserRoles.cs

---
+++ A5/Authentication/UserRoles.cs
@@ -0,0 +1,11 @@
+namespace A5.Authentication
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Publisher = "Publisher";
+        public const string Approver = "Approvaer";
+        public const string Requester = "Requester";
+        public const string User = "User";
+    }
+}
