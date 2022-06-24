ca9bf183c72567675ec755905bebb2c8a9bd092e
Jeevanantham N <jeevanantham3006@gmail.com>
2022-06-24 11:58:28 +0530

    Create Roles.cs

---
+++ A5/Models/Roles.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A5.Models
+{
+    public class Roles
+    {
+        public int RoleID{ get; set; }
+        public string RoleName{ get; set; }
+    }
+}
