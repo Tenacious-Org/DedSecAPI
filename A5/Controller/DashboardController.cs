9a25c3e84b72b9cc844fa2eea4d4f6c2ddd4d20c
Jeevanantham N <jeevanantham3006@gmail.com>
2022-07-07 13:35:19 +0530

    Create DashboardController.cs

---
+++ A5/Controller/DashboardController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using A5.Data;
+using A5.Data.Repository;
+using A5.Data.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace A5.Controller
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly ILogger<DashboardController> _logger;
+        private readonly AppDbContext _context;
+        private readonly DashboardService _dash;
+
+        public DashboardController(ILogger<DashboardController> logger, DashboardService dash, AppDbContext context)
+        {
+            _logger = logger;
+            _dash = dash;
+            _context = context;
+        }
+
+        [HttpGet("Get All Winners")]
+        public ActionResult GetPublisherDashboard()
+        {
+            try
+            {
+                var data = _dash.GetAllWinners();
+                return Ok(data);
+            }
+            catch(Exception exception)
+            {
+                return BadRequest($"Error : {exception.Message}");
+            }
+        }   
+    }
+}
