309200b279148ab18c726cefdbe42008cd222ee8
Jeevanantham N <jeevanantham3006@gmail.com>
2022-07-15 15:23:53 +0530

    Update AwardRepository.cs

--- A5/Data/Repository/AwardRepository.cs
+++ A5/Data/Repository/AwardRepository.cs
@@ -255,6 +255,95 @@ namespace A5.Data.Repository
             }
         }
 
+        public IEnumerable<Award> GetAllAwardwise(int id)
+        {
+            try
+            {
+                var award = _context.Set<Award>()
+                    .Include("Awardee")
+                    .Include("Awardee.Designation")
+                    .Include("Awardee.Designation.Department")
+                    .Include("Awardee.Designation.Department.Organisation")
+                    .Include("Awardee.ReportingPerson")
+                    .Include("Awardee.ReportingPerson.ReportingPerson")
+                    .Include("Awardee.HR")
+                    .Include("AwardType")
+                    .Include("Status")
+                    .Where(nameof => nameof.AwardTypeId == id && nameof.StatusId == 4)
+                    .ToList();
+                return award;
+            }
+             catch(ValidationException exception)
+            {
+                _logger.LogError("AwardRepository : GetAllbyOrgwise(int id) : (Error:{Message}",exception.Message);
+                throw;
+            }
+            catch (Exception exception){
+              _logger.LogError("Error: {Message}",exception.Message);
+              throw;
+            }
+        }
+
+        public IEnumerable<Award> GetAllOrgAndAwardwise(int orgid, int awdid)
+        {
+            try
+            {
+                var award = _context.Set<Award>()
+                    .Include("Awardee")
+                    .Include("Awardee.Designation")
+                    .Include("Awardee.Designation.Department")
+                    .Include("Awardee.Designation.Department.Organisation")
+                    .Include("Awardee.ReportingPerson")
+                    .Include("Awardee.ReportingPerson.ReportingPerson")
+                    .Include("Awardee.HR")
+                    .Include("AwardType")
+                    .Include("Status")
+                    .Where(nameof => nameof.Awardee.Designation.Department.Organisation.Id == orgid && nameof.AwardTypeId == awdid && nameof.StatusId == 4)
+                    .ToList();
+                return award;
+            }
+             catch(ValidationException exception)
+            {
+                _logger.LogError("AwardRepository : GetAllbyOrgwise(int id) : (Error:{Message}",exception.Message);
+                throw;
+            }
+            catch (Exception exception){
+              _logger.LogError("Error: {Message}",exception.Message);
+              throw;
+            }
+        }
+
+        public IEnumerable<Award> GetAllDateWise(int orgid, int awdid, DateTime start, DateTime end)
+        {
+            try
+            {
+                var award = _context.Set<Award>()
+                    .Include("Awardee")
+                    .Include("Awardee.Designation")
+                    .Include("Awardee.Designation.Department")
+                    .Include("Awardee.Designation.Department.Organisation")
+                    .Include("Awardee.ReportingPerson")
+                    .Include("Awardee.ReportingPerson.ReportingPerson")
+                    .Include("Awardee.HR")
+                    .Include("AwardType")
+                    .Include("Status")
+                    .Where(nameof   => nameof.Awardee.Designation.Department.Organisation.Id == orgid 
+                                    && nameof.AwardTypeId == awdid 
+                                    && nameof.StatusId == 4
+                                    && (nameof.UpdatedOn >= start.Date && nameof.UpdatedOn <= end.Date))
+                    .ToList();
+                return award;
+            }
+             catch(ValidationException exception)
+            {
+                _logger.LogError("AwardRepository : GetAllbyOrgwise(int id) : (Error:{Message}",exception.Message);
+                throw;
+            }
+            catch (Exception exception){
+              _logger.LogError("Error: {Message}",exception.Message);
+              throw;
+            }
+        }
         
        
    }
